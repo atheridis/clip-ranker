a4cdf75f1be84ce42155d736c26e772f9b7bde2c
Georgios Atheridis <georgios@atheridis.org>
2023-01-23 10:43:11 +0000

    Added AGPLv3 License

--- core/manager/views.py
+++ core/manager/views.py
@@ -1,4 +1,19 @@
-from django.http import HttpResponse
+"""
+Copyright (C) 2023  Georgios Atheridis <georgios@atheridis.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from django.contrib.admin.views.decorators import staff_member_required
